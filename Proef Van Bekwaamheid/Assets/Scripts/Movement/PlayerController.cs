85a3e60efc67903d3b15d1223380b9c4b3dba7ee
LeonKDev <Leonkruijer@outlook.com>
2026-03-23 12:36:32 +0100

    working movement with networking

--- Proef Van Bekwaamheid/Assets/Scripts/Movement/PlayerController.cs
+++ Proef Van Bekwaamheid/Assets/Scripts/Movement/PlayerController.cs
@@ -2,39 +2,61 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : NetworkBehaviour
 {
+    [SerializeField] private Rigidbody rb;
     public float speed = 1f;
     public float rotationSpeed = 0.15f;
     private Vector2 _move;
+    private bool _pcAssigned;
+
+    private void Start()
+    {
+        Debug.Log(OwnerClientId);
+        InvokeRepeating(nameof(AssignPlayerController), 0.1f, 0.1f);
+    }
 
     private void Update()
     {
+        Debug.Log(IsOwner);
+
         if (!IsOwner)
             return;
 
         MovePlayer();
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (!IsOwner)
+            return;
+
         _move = context.ReadValue<Vector2>();
     }
 
     public void MovePlayer()
     {
-        MoveServerRpc();
-    }
+        if (!IsOwner && !_pcAssigned)
+            return;
 
-    [ServerRpc]
-    public void MoveServerRpc()
-    {
         Vector3 movement = new Vector3(_move.x, 0f, _move.y);
 
         if (movement == Vector3.zero)
             return;
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+        rb.transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    private void AssignPlayerController()
+    {   
+        _pcAssigned = true;
+        CancelInvoke();
+    }
+
+    public void ResetController()
+    {
+        _pcAssigned = false;
+        InvokeRepeating(nameof(AssignPlayerController), 0.1f, 0.1f);
     }
 }
