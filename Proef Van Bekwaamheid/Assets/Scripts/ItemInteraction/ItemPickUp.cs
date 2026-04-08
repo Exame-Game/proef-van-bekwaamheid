54fc05fdde31a69d7ab65c7eb181f7f05857a491
LeonKDev <Leonkruijer@outlook.com>
2026-04-08 15:26:55 +0200

    Item Pick up and Drop

--- Proef Van Bekwaamheid/Assets/Scripts/ItemInteraction/ItemPickUp.cs
+++ Proef Van Bekwaamheid/Assets/Scripts/ItemInteraction/ItemPickUp.cs
@@ -1,16 +1,60 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ItemPickUp : MonoBehaviour
+public class ItemPickUp : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputActionAsset inputActions;
+    public LayerMask PickUpLayer;
+    public Vector3 Direction { get; set; }
+    public Transform PickUpPoint;
+
+    private GameObject ItemHolding;
+    private InputAction interactAction;
+
+    private void Awake()
     {
-        
+        interactAction = inputActions.FindAction("PickUp");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!IsOwner) return;
+
+        if (interactAction.WasPressedThisFrame())
+        {
+            if (ItemHolding)
+            {
+                ItemHolding.transform.parent = null;
+                ItemHolding.transform.position = transform.position + Direction;
+                if (ItemHolding.TryGetComponent<Rigidbody>(out Rigidbody heldRb))
+                {
+                    heldRb.isKinematic = false;
+                    heldRb.useGravity = true;
+                    heldRb.detectCollisions = true;
+                }
+                ItemHolding = null;
+            }
+            else
+            {
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 2f, PickUpLayer);
+                if (colliders.Length > 0 && PickUpPoint.childCount == 0)
+                {
+                    ItemHolding = colliders[0].gameObject;
+                    if (ItemHolding.TryGetComponent<Rigidbody>(out Rigidbody rb))
+                    {
+                        rb.isKinematic = true;
+                        rb.useGravity = false;
+                        rb.detectCollisions = false;
+                    }
+                    ItemHolding.transform.parent = PickUpPoint;
+                    ItemHolding.transform.localPosition = Vector3.zero;
+                    ItemHolding.transform.localRotation = Quaternion.identity;
+                }
+            }
+        }
     }
+
+    void OnEnable() { interactAction.Enable(); }
+    void OnDisable() { interactAction.Disable(); }
 }
