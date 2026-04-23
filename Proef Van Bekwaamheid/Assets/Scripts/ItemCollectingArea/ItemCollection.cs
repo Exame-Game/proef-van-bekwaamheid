ac18f3ad4244d5198c9918edd156d3352458489b
LeonKDev <Leonkruijer@outlook.com>
2026-04-23 23:59:11 +0200

    WIP

--- Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/ItemCollection.cs
+++ Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/ItemCollection.cs
@@ -1,5 +1,3 @@
-using DG.Tweening;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -8,41 +6,39 @@ using UnityEngine;
 public class ItemCollection : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _inventoryText;
-    [SerializeField] private Collider _collider;
 
-    [SerializeField] private DOTweenAnimation _tween;
+    private Collider _collider;
+    private InventoryData _inventoryData;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+        _inventoryData = new InventoryData();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer != LayerMask.NameToLayer("PickUp"))
+        if (other.gameObject.layer != LayerMask.NameToLayer("PickUp")) 
             return;
 
-        InventoryData.Instance.AddItem(other.gameObject.GetComponent<Item>().data);
+        _inventoryData.AddItem(other.gameObject.GetComponent<Item>().data);
         UpdateInventoryText();
-        _tween = other.gameObject.GetComponent<DOTweenAnimation>();
-        StartCoroutine(Tween());
-        Destroy(other.gameObject.GetComponent<Item>());
     }
 
     private void UpdateInventoryText()
     {
         string text = "";
-        foreach (KeyValuePair<Rarity, List<ItemData>> entry in InventoryData.Instance.ItemsByRarity.OrderBy(kvp => (int)kvp.Key))
-        {
-            if (entry.Value.Count == 0)
-                continue;
 
+        foreach (KeyValuePair<Rarity, List<ItemData>> entry in _inventoryData.ItemsByRarity.OrderBy(kvp => (int)kvp.Key))
+        {
             text += $"<b>{entry.Key}</b>\n";
+
             foreach (ItemData item in entry.Value)
                 text += $"  {item.Name}\n";
 
             text += "\n";
         }
+
         _inventoryText.text = text;
-    }
-    private IEnumerator Tween()
-    {
-        _tween.DOPlay();
-        yield return new WaitForSeconds(_tween.duration);
     }
 }
