e62d73831bfc888fe9736a7181c9121fa067b985
FlorianBruijn <114649536+FlorianBruijn@users.noreply.github.com>
2026-05-17 15:02:56 +0200

    feat: made the dropsing apear when near dropbox with item in hand and disapear when leaving

--- Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/DropIcon.cs
+++ Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/DropIcon.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class DropIcon : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    [SerializeField] private Renderer renderer;
+
+    void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.layer == 9)
-        {
-            
-        }
+        if (other.gameObject.layer != 9)
+            return;
+        ItemPickUp pickUp = other.GetComponent<ItemPickUp>();
+        if (pickUp.State != ItemPickUp.PickUpState.Holding && pickUp.State != ItemPickUp.PickUpState.WaitingForRelease)
+            return;
+        renderer.enabled = true;    
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != 9)
+            return;
+        renderer.enabled = false;
     }
 }
