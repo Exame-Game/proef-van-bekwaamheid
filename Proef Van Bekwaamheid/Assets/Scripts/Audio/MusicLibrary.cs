361f8c66b6d4a74ff0c85e18a20e4b5cc426a3e9
LeonKDev <Leonkruijer@outlook.com>
2026-05-18 15:42:06 +0200

    added scripts and new sounds

---
+++ Proef Van Bekwaamheid/Assets/Scripts/Audio/MusicLibrary.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct MusicTrack
+{
+    public string trackName;
+    public AudioClip clip;
+}
+
+public class MusicLibrary : MonoBehaviour
+{
+    [SerializeField] private MusicTrack[] musicTracks;
+    
+    public AudioClip GetClipFromName(string trackName)
+    {
+        foreach (MusicTrack track in musicTracks)
+        {
+            if (track.trackName == trackName)
+                return track.clip;
+        }
+
+        Debug.LogWarning("Music track not found: " + trackName);
+        return null;
+    }
+}
