0da8e6cf2604086b4d1cf8ce1fddd5290f12e664
LeonKDev <Leonkruijer@outlook.com>
2026-05-20 21:54:52 +0200

    fixed null reference

--- Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/ItemCollection.cs
+++ Proef Van Bekwaamheid/Assets/Scripts/ItemCollectingArea/ItemCollection.cs
@@ -9,7 +9,6 @@ public class ItemCollection : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _inventoryText;
     [SerializeField] private Collider _collider;
-
     private DOTweenAnimation _tween;
 
     private void OnTriggerEnter(Collider other)
@@ -17,15 +16,31 @@ public class ItemCollection : MonoBehaviour
         if (other.gameObject.layer != LayerMask.NameToLayer("PickUp"))
             return;
 
-        ItemData data = other.gameObject.GetComponent<Item>().data;
+        Item itemComponent = other.gameObject.GetComponent<Item>();
+        if (itemComponent == null)
+        {
+            Debug.LogWarning($"Item collider entered but no Item component found on {other.gameObject.name}");
+            return;
+        }
+
+        ItemData data = itemComponent.data;
         if (data == null)
             return;
 
         InventoryData.Instance.AddItem(data);
         UpdateInventoryText();
-        _tween = other.gameObject.GetComponent<DOTweenAnimation>();
-        StartCoroutine(Tween());
-        Destroy(other.gameObject.GetComponent<Item>());
+
+        DOTweenAnimation tween = other.gameObject.GetComponent<DOTweenAnimation>();
+        if (tween != null)
+        {
+            StartCoroutine(Tween(tween, other.gameObject));
+        }
+        else
+        {
+            other.gameObject.SetActive(false);
+        }
+
+        Destroy(itemComponent);
     }
 
     public void UpdateInventoryText()
@@ -35,20 +50,21 @@ public class ItemCollection : MonoBehaviour
         {
             if (entry.Value.Count == 0)
                 continue;
-
             text += $"<b>{entry.Key}</b>\n";
             foreach (ItemData item in entry.Value)
                 text += $"  {item.Name}\n";
-
             text += "\n";
         }
         _inventoryText.text = text;
     }
-    private IEnumerator Tween()
+
+    private IEnumerator Tween(DOTweenAnimation tween, GameObject targetObject)
     {
-        _tween.DOPlay();
-        yield return new WaitForSeconds(_tween.duration);
-        _tween.gameObject.SetActive(false);
+        tween.DOPlay();
+        Debug.Log(tween.duration);
+        yield return new WaitForSeconds(tween.duration);
+        if (targetObject != null)
+            targetObject.SetActive(false);
     }
 
     public void ResetInventory()
